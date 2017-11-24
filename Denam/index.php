93ead29755e7dbbd77f0ff3af9c31184e2659a2f
Albert74 <33966831+Albert74@users.noreply.github.com>
2017-11-24 17:24:13 -0400

    Add files via upload

---
+++ Denam/index.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/main.css">
+    <meta name="viewport"  content="width=device-width, initail-scale=1,user-scalable">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+      <a href="inde.php" class="navbar-brand">DENAM </a>
+      <ul class="nav navbar-nav">
+        <li class="dropdown">
+          <a href="#" class="dropdonw-toggle" data-toggle="dropdown">Clothing<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Men</a></li>
+            <li><a href="#">Women</a></li>
+            <li><a href="#">Children</a></li>
+        </ul>
+
+    </ul>
+    <ul class="nav navbar-nav">
+      <li class="dropdown">
+        <a href="#" class="dropdonw-toggle" data-toggle="dropdown">Food<span class="caret"></span></a>
+        <ul class="dropdown-menu" role="menu">
+          <li><a href="#">Vegetables</a></li>
+          <li><a href="#">Fruits</a></li>
+          <li><a href="#">Meat & Fish</a></li>
+      </ul>
+    </ul>
+    <ul class="nav navbar-nav">
+      <li class="dropdown">
+        <a href="#" class="dropdonw-toggle" data-toggle="dropdown">Electronics<span class="caret"></span></a>
+        <ul class="dropdown-menu" role="menu">
+          <li><a href="#">Phones</a></li>
+          <li><a href="#">Laptopss</a></li>
+          <li><a href="#">Accessories</a></li>
+      </ul>
+  </div>
+</nav>
+
+<section class="page-title page-title -1 image-bg overlay pixelz-dark parallax">
+  <div class="background-image-holder fadeIn " style="tranform:translate3d"(0px,0px,0px);
+  <div class="container">
+  <!--  ::before-->
+    <div class="row">
+    <!--  ::before-->
+      <div class="col-sm-12 text-center">
+        <h2 class="uppercase mb12">DENAM</h2>
+        <p class="lead">The Best Delivery store in Cape Coast</p>
+      </div>
+    <!--  ::after-->
+    </div>
+    <!--::after-->
+  </div>
+</section>
+
+<!--left side bar -->
+<div class="container-fluid">
+  <div class="col-md-2"> </div>
+
+<!--main content -->
+<div class="col-md-8">
+<div class="row">
+  <h2 class="text-center">Featured Products</h2>
+    <div class="col-md-3">
+      <h4>Shirts</h4>
+    <!--  <img src="shirt1" alt="shirt1" class="img-thumbnail">-->
+      <img src-data="image/products/shirt1.jpg"  class="img-thumbnail"  alt="shirt"  style= "width:200px;  height:200px;"/>
+        <p  class="list-price text-danger">List Price <s>¢150</s></p>
+      <p class="price">Our Price: ¢100</p>
+      <button type="button" class="btn-sm btn-success" data-toggel="modal" data-target="details-1">
+        Details</button>
+    </div>
+    <div class="col-md-3">
+      <h4>Shoes</h4>
+      <img src=image/products/shirt2.jpg"" alt="Shirts" class="img-thumbnail" style="width:200px; height:200px;"/>
+      <p  class="list-price text-danger">List Price <s>¢160</s></p>
+      <p class="price">Our Price: ¢90</p>
+      <button type="button" class="btn-sm btn-success" data-toggel="modal" data-target="details-1">
+        Details</button>
+    </div>
+
+    <div class="col-md-3">
+      <h4>T-Shirts</h4>
+      <img src="shirt2.jpg" alt="Shirts" class="img-thumbnail" style="width:200px; height:200px;"/>
+      <p  class="list-price text-danger">List Price <s>¢115</s></p>
+      <p class="price">Our Price: ¢111</p>
+      <button type="button" class="btn-sm btn-success" data-toggel="modal" data-target="details-1">
+        Details</button>
+    </div>
+    <div class="col-md-3">
+      <h4>Dress</h4>
+      <img src="shirt2.jpg" alt="Shirts" class="img-thumbnail"style="width:200px; height:200px;"/>
+      <p  class="list-price text-danger">List Price <s>¢80</s></p>
+      <p class="price">Our Price: ¢50</p>
+      <button type="button" class="btn-sm btn-success" data-toggel="modal" data-target="details-1">
+        Details</button>
+    </div>
+    <div class="col-md-3">
+      <h4>Dress</h4>
+      <img src="shirt2.jpg" alt="Shirts" class="img-thumbnail"style="width:200px; height:200px;"/>
+      <p  class="list-price text-danger">List Price <s>¢80</s></p>
+      <p class="price">Our Price: ¢50</p>
+      <button type="button" class="btn-sm btn-success" data-toggel="modal" data-target="details-1">
+        Details</button>
+    </div>
+    <div class="col-md-3">
+      <h4>Dress</h4>
+      <img src="shirt2.jpg" alt="Shirts" class="img-thumbnail"style="width:200px; height:200px;"/>
+      <p  class="list-price text-danger">List Price <s>¢80</s></p>
+      <p class="price">Our Price: ¢50</p>
+      <button type="button" class="btn-sm btn-success" data-toggel="modal" data-target="details-1">
+        Details</button>
+    </div>
+    <div class="col-md-3">
+      <h4>Dress</h4>
+      <img src="shirt2.jpg" alt="Shirts" class="img-thumbnail"style="width:200px; height:200px;"/>
+      <p  class="list-price text-danger">List Price <s>¢80</s></p>
+      <p class="price">Our Price: ¢50</p>
+      <button type="button" class="btn-sm btn-success" data-toggel="modal" data-target="details-1">
+        Details</button>
+    </div>
+    <div class="col-md-3">
+      <h4>Dress</h4>
+      <img src="shirt2.jpg" alt="Shirts" class="img-thumbnail"style="width:200px; height:200px;"/>
+      <p  class="list-price text-danger">List Price <s>¢80</s></p>
+      <p class="price">Our Price: ¢50</p>
+      <button type="button" class="btn-sm btn-success" data-toggel="modal" data-target="details-1">
+        Details</button>
+    </div>
+    <div class="col-md-3">
+      <h4>Dress</h4>
+      <img src="shirt2.jpg" alt="Shirts" class="img-thumbnail"style="width:200px; height:200px;"/>
+      <p  class="list-price text-danger">List Price <s>¢80</s></p>
+      <p class="price">Our Price: ¢50</p>
+      <button type="button" class="btn-sm btn-success" data-toggel="modal" data-target="details-1">
+        Details</button>
+    </div>
+    <div class="col-md-3">
+      <h4>Dress</h4>
+      <img src=shirt2.jpg"" alt="Shirts" class="img-thumbnail"style="width:200px; height:200px;"/>
+      <p  class="list-price text-danger">List Price <s>¢80</s></p>
+      <p class="price">Our Price: ¢50</p>
+      <button type="button" class="btn-sm btn-success" data-toggel="modal" data-target="details-1">
+        Details</button>
+    </div>
+    <div class="col-md-3">
+      <h4>Dress</h4>
+      <img src="shirt2.jpg" alt="Shirts" class="img-thumbnail"style="width:200px; height:200px;"/>
+      <p  class="list-price text-danger">List Price <s>¢80</s></p>
+      <p class="price">Our Price: ¢50</p>
+      <button type="button" class="btn-sm btn-success" data-toggel="modal" data-target="details-1">
+        Details</button>
+    </div>
+    <div class="col-md-3">
+      <h4>Dress</h4>
+      <img src="ohene.jpg" alt="Shirts" class="img-thumbnail"style="width:200px; height:200px;"/>
+      <p  class="list-price text-danger">List Price <s>¢80</s></p>
+      <p class="price">Our Price: ¢50</p>
+      <button type="button" class="btn-sm btn-success" data-toggel="modal" data-target="details-1">
+        Details</button>
+    </div>
+
+
+</div>
+ </div>
+
+<!--rigth side bar-->
+<div class="col-md-2"> </div>
+
+</div>
+<footer class="text-center" id="footer"> &copy; Albert @ Asare </footer>
+
+
+<div class="modal fade details-1" id="details-1" tabindex="-1" role="dialog" aria-labelledby="details-1" aria-hidden="true">
+<div class="modal-dialog modal-lg">
+  <div class="modal-Content">
+    <div class="modal-header">
+        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <h4 class="modal-title text-center">Jeans</h4>
+    </div>
+      <div class="modal-body">
+        <div class="container-fluid">
+         <div class="row">
+           <div class="col-sm-6">
+           <div class="center-block"></div>
+           <img src="images/products/shirt1.jpg" alt="shirt" class="details img-responsive">
+         </div>
+       </div>
+         <div class="col-sm-6">
+           <h4>Details</h4>
+           <p> This shirt will look great on you</p>
+           <hr>
+           <p> Price: ¢155</p>
+           <p> Brand: Polo</p>
+           <form action="add_cart.php" method="post">
+             <div class="form-group">
+               <div class="col-xs-3">
+                 <label for="qauntity">Quantity:</label>
+                 <input type="text" class="form-control" id="qauntity" name="quantity">
+               </div><div class="col-xs-9"></div>
+                <p>Available:3 </p>
+           </div><br><br>
+           <div class="form-group">
+             <label for="size">Size</label>
+             <select name="size" id="size" class="form-control">
+               <option vlaue=""></option>
+               <option vlaue="30">30</option>
+               <option vlaue="32">32</option>
+               <option vlaue="34">34</option>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button class="btn-btn-default" data-dismiss="modal">Close</button>
+        <buttton class="btn btn-warning" type="submit"><span class="glyphicon glyphicon-shopping-art">Add to cart</span>
+      </div>
+  </div>
+    </div>
+  </div>
+
+
+<script>
+  jQuery(window).scroll(function(){
+    var vscroll =jQuery(this).scrollTop();
+    jquery('#logotext').css({
+      "tranform":"translate(0px, "+vscroll/2+"px)"
+    });
+    var vscroll =jQuery(this).scrollTop();
+    jquery('#back-flower').css({
+      "tranform":"translate(0px, - "+vscroll/2+"px)"
+    });
+    var vscroll =jQuery(this).scrollTop();
+    jquery('#fore-flower').css({
+      "tranform":"translate(0px, -"+vscroll/2+"px)"
+    });
+
+  });
+</script>
+</body>
+</html>
