9baf558c398303acd92cac2969d56f68c0a53b09
Albert Asare <33966831+Albert74@users.noreply.github.com>
2017-11-26 16:51:28 -0400

    grouping of files...

---
+++ includes/LeftSideBar.php
@@ -0,0 +1,2 @@
+<!--Left side Bar -->
+<div class="col-md-2">Left side Bar </div>
---
+++ includes/headfull.php
@@ -0,0 +1,20 @@
+<section class="page-title page-title -1 image-bg overlay pixelz-dark parallax">
+  <div class="background-image-holder fadeIn " style="tranform:translate3d"(0px,0px,0px);
+
+
+<div class="container">
+<!--  ::before-->
+  <div class="row">
+  <!--  ::before-->
+    <div class="col-sm-12 text-center">
+      <h2 class="uppercase mb-12">DENAM</h2>
+      <p class="lead">The Best Delivery store in Cape Coast</p>
+    </div>
+  <!--  ::after-->
+  </div>
+  <!--::after-->
+</div>
+</section>
+
+<!--left side bar -->
+<div class="container-fluid">
